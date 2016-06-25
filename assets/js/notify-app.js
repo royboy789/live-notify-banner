2417a1b57cf4322cd6782dfd2f6590605fc19d07
Roy Sivan <roy@guavaworks.com>
2016-06-24 21:00:21 -0700

    unbinding- won't work with FED (can see typing)

--- assets/js/notify-app.js
+++ assets/js/notify-app.js
@@ -12,18 +12,15 @@ var live_notify_app = live_notify_app || {};
                templateUrl: notify_local.template_directory + '/admin.html',
                controller: ['$scope', '$firebaseObject', '$timeout', function( $scope, $firebaseObject, $timeout ) {
 
-                   $scope.notify = {
-                       active: '0'
-                   };
                    $scope.save_notify = false;
 
-                   var ref = new Firebase('https://cc-notifications.firebaseio.com'),
-                       syncObject = $firebaseObject( ref );
-                   syncObject.$bindTo( $scope, 'notify' );
+                   var ref = new Firebase('https://cc-notifications.firebaseio.com');
+                   $scope.notify = $firebaseObject( ref );
 
 
                    $scope.updateNotification = function() {
                        console.log('updating...');
+                       $scope.notify.$save($scope.notify);
                        $scope.save_notify = true;
 
                        $timeout(function(){
